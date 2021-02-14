765523c83df1410419f9a913a4187c2e7e6d0998
unknown <cherylliu719@gmail.com>
2021-02-13 21:14:46 -0800

    this is the commit from cheryl

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,7 @@
 import com.leapcode.service.SpeakerService;
 import com.leapcode.service.SpeakerServiceImpl;
 
+//This is my change on the forth line.
 public class Application {
     public static void main(String[] args){
         SpeakerService service = new SpeakerServiceImpl();
